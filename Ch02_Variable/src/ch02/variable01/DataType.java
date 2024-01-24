3522a94c8f4382112501dc2fe1a79ec1b81dffdb
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 15:45:41 +0900

    Java 데이터 타입

---
+++ Ch02_Variable/src/ch02/variable01/DataType.java
@@ -0,0 +1,23 @@
+package ch02.variable01;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 100;
+		double dnum = 3.14;
+		char ch = 'a';
+		String name = "홍길동";
+		boolean isRun = true;
+		String str = "?";
+		
+		System.out.println(num);
+		System.out.println(dnum);
+		System.out.println(ch);
+		System.out.println(name);
+		System.out.println(isRun);
+		System.out.println(str);
+		
+	}
+
+}
