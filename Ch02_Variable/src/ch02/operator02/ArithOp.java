298efdb2824f9071093259df78d360e06bc371c9
dyddks <dyddks0115@naver.com>
2024-01-24 19:52:37 +0900

    사칙연산

---
+++ Ch02_Variable/src/ch02/operator02/ArithOp.java
@@ -0,0 +1,18 @@
+package ch02.operator02;
+
+public class ArithOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num0 = 10;
+		int num1 = 3;
+
+		System.out.println(num0 + num1);
+		System.out.println(num0 - num1);
+		System.out.println(num0 * num1);
+		System.out.println(num0 / num1);
+		System.out.println(num0 % num1);
+		System.out.println();
+	}
+
+}
