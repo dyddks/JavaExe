3bbae1e5c232872a7ed895c8121caee7b9e1f061
dyddks <dyddks0115@naver.com>
2024-01-24 19:52:49 +0900

    논리연산자 실습

---
+++ Ch02_Variable/src/ch02/operator04/compOperator.java
@@ -0,0 +1,17 @@
+package ch02.operator04;
+
+public class compOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 100;
+		int n2 = 10;
+		int n3 = 100;
+		
+		System.out.println(n1>n2);
+		System.out.println(n1<n2);
+		System.out.println(n1 == n3);
+		System.out.println(n1>=n3);
+	}
+
+}
