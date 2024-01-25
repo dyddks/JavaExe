9816e254c252ab7214ba90505ae8c36e1fa8a657
dyddks <dyddks0115@naver.com>
2024-01-25 11:53:11 +0900

    복합대입연산 실습

---
+++ Ch02_Variable/src/ch02/assignmentOperator10/AssignmentOperator.java
@@ -0,0 +1,19 @@
+package ch02.assignmentOperator10;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 0;
+		num = num + 10;
+		System.out.println(num);
+		num = num + 20;
+		System.out.println(num);
+
+		num += 30;
+		System.out.println(num);
+		num += 40;
+		System.out.println(num);
+	}
+
+}
