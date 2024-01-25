22794d67d42dfa0fba46f6ee9c290993ec06177e
dyddks <dyddks0115@naver.com>
2024-01-25 16:10:39 +0900

    while문 실습

---
+++ Ch03_Control/src/ch03/while08/WhileEx.java
@@ -0,0 +1,14 @@
+package ch03.while08;
+
+public class WhileEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while(i<10) {
+			i++;
+			System.out.print(i+ " ");
+		}
+	}
+
+}
