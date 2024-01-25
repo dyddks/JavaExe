5012bcf77a039cdaf1dba21ee555b1c489b269da
dyddks <dyddks0115@naver.com>
2024-01-25 12:51:04 +0900

    연산자 우선순위 실습

---
+++ Ch02_Variable/src/ch02/orderOperator13/OrderOperator.java
@@ -0,0 +1,14 @@
+package ch02.orderOperator13;
+
+public class OrderOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result = 10 + 50 * 3;
+		System.out.println(result);
+		
+		result = (10 + 50) * 3;
+		System.out.println(result);
+	}
+
+}
