f196870a6994761aa7915165ed880cf8a00c6d6a
dyddks <dyddks0115@naver.com>
2024-01-25 15:56:03 +0900

    Switch Expressions 예제 실습

---
+++ Ch03_Control/src/ch04/sec03/SwitchExpressionExample.java
@@ -0,0 +1,30 @@
+package ch04.sec03;
+
+public class SwitchExpressionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char grade = 'B';
+		
+		// 표현식을 이용한 switch문
+		switch(grade ) {
+			case 'A', 'a' -> {
+				System.out.println("우수회원");
+			}
+			case 'B', 'b' -> {
+				System.out.println("일반회원");
+			}
+			default -> {
+				System.out.println("손님");
+			}
+		}
+		
+		// 중괄호 생략
+		switch(grade) {
+			case 'A', 'a' -> System.out.println("우수회원");
+			case 'B', 'b' -> System.out.println("일반회원");
+			default -> System.out.println("손님");
+		}
+	}
+
+}
