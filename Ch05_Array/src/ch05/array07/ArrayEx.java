6a71d120cdee21f92264297f9f7973d32c13efdc
dyddks <dyddks0115@naver.com>
2024-01-26 17:23:21 +0900

    sort예제

---
+++ Ch05_Array/src/ch05/array07/ArrayEx.java
@@ -0,0 +1,17 @@
+package ch05.array07;
+
+import java.util.Arrays;
+
+public class ArrayEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] scores = {100,88,95,67,75};
+		
+		Arrays.sort(scores);
+	
+		System.out.println(Arrays.toString(scores));
+	}
+
+}
+
