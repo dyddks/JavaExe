a3d4fba5112a54f08f72adb83ac3ad86ab8e3840
dyddks <dyddks0115@naver.com>
2024-01-29 17:27:20 +0900

    Enum 예제

---
+++ Ch07_Enum/src/ch07/enum01/ch05/sec12/Week.java
@@ -0,0 +1,13 @@
+package ch07.enum01.ch05.sec12;
+
+// 프로그램의 가독성을 높여주는 역할
+// 내부적으로 차례대로 0부터 1씩 증가하는 역할을 한다.
+public enum Week {
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
