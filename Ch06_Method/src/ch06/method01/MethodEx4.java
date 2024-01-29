b8fa3ff33bc7f1a4861bf8ea692f313d9e393098
dyddks <dyddks0115@naver.com>
2024-01-29 15:31:25 +0900

    출력 메소드 분리

---
+++ Ch06_Method/src/ch06/method01/MethodEx4.java
@@ -0,0 +1,53 @@
+package ch06.method01;
+
+import java.util.Scanner;
+
+public class MethodEx4 {
+	// 계산하는 메소드
+	public static int calcArith(int num0, int num1, String op) {
+		int result = 0;
+	
+		switch (op) {
+		case "+":
+			result = num0 + num1;
+			break;
+		case "-":
+			result = num0 - num1;
+			break;
+		case "*":
+			result = num0 * num1;
+			break;
+		case "/":
+			result = num0 / num1;
+			break;
+		case "%":
+			result = num0 % num1;
+			break;
+		default: 
+			System.out.println("연산이 불가능 합니다.");
+		}
+		return result;
+	}
+
+	public static void viewResult(int result) {
+		// 3) 결과값 출력
+		System.out.println("결과값은" + result + "입니다.");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		// 1) 입력받는 부분
+		System.out.println("1번째 정수 입력 >>");
+		int num0 = sc.nextInt();
+		System.out.println("2번째 정수 입력 >>");
+		int num1 = sc.nextInt();
+		System.out.println("연산자 입력(+, -, *, /, %) >> ");
+		String op = sc.next();
+		
+		int result = calcArith(num0, num1, op);
+		viewResult(result);
+	}
+
+}
