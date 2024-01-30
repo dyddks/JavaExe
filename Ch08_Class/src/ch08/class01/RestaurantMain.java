364fbd2e6345a6dadf45fffdfd18e4afeb37eb1e
dyddks <dyddks0115@naver.com>
2024-01-30 14:30:44 +0900

    RestaurantMain 클래스

---
+++ Ch08_Class/src/ch08/class01/RestaurantMain.java
@@ -0,0 +1,15 @@
+package ch08.class01;
+
+public class RestaurantMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Restaurant rest = new Restaurant();
+		
+		rest.viewMenu();
+		rest.selectFood();
+		rest.deliveryFood();
+		rest.keyboardEnd();
+	}
+
+}
