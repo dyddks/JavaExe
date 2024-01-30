b91112bd695be73916e7359b1dc30765bedb62dc
dyddks <dyddks0115@naver.com>
2024-01-30 09:26:28 +0900

    min, max메소드 수정

--- Ch06_Method/src/ch06/method01/Resolve1.java
+++ Ch06_Method/src/ch06/method01/Resolve1.java
@@ -3,18 +3,20 @@ package ch06.method01;
 import java.util.Scanner;
 
 public class Resolve1 {
-
+	//작은값 반환 메소드
 	public static int min(int[] num) {
-		int min = Math.min(num[0], num[1]);
+		int min = num[0]<num[1]? num[0]: num[1];
 
 		return min;
 	}
 	
+	//큰값 반환 메소드
 	public static int max(int[] num) {
-		int max = Math.max(num[0], num[1]);
+		int max = num[0]>num[1]? num[0]: num[1];
 		return max;
 	}
 	
+	// 정수 입력 메소드
 	public static int[] input() {
 		int[] num = new int[2];
 		Scanner sc = new Scanner(System.in);
@@ -24,9 +26,12 @@ public class Resolve1 {
 		
 		return num;
 	}
+	
+	// 결과 출력 메소드
 	public static void view(int min, int max) {
 		System.out.printf("작은값: %d, 큰값: %d", min, max);
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] num = input();
