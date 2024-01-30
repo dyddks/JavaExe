03c8147970baa382f475550f42b0ed45c8dd440b
dyddks <dyddks0115@naver.com>
2024-01-30 14:56:27 +0900

    주석 추가

--- Ch08_Class/src/ch08/class03/Arith.java
+++ Ch08_Class/src/ch08/class03/Arith.java
@@ -1,5 +1,12 @@
 package ch08.class03;
 
+/* static 메소드는 객체를 만들지않아도 메모리에 자동으로 올라간다.
+ * 
+ * [용도]
+ * 범용적인 기능의 함수를 만들 때
+ * 어느 곳에서나 쉽게 사용하고 싶은 함수를 만들 때
+ * 객체에 따라 변수의 값의 적용을 다르게 받지 않아도 될 때
+ * */
 public class Arith {
 	public static int add(int num0, int num1) {
 		return num0 + num1;
