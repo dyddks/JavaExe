70c3d8d2a7eeb560235b39edd44131def921656c
dyddks <dyddks0115@naver.com>
2024-01-31 17:55:54 +0900

    Resolve1 문제 풀이

---
+++ Ch09_Extends/src/ch09/resolve07/Resolve1_Mul.java
@@ -0,0 +1,14 @@
+package ch09.resolve07;
+
+public class Resolve1_Mul {
+	private int a;
+	private int b;
+	
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	public int calculate() {
+		return a*b;
+	}
+}
