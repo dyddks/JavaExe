632ebf18898f3603e5ade3dd7b5bc28fe44b50fa
dyddks <dyddks0115@naver.com>
2024-01-31 15:29:44 +0900

    필드 상속 예제

---
+++ Ch09_Extends/src/ch09/extends02/Father.java
@@ -0,0 +1,9 @@
+package ch09.extends02;
+
+public class Father extends GrandFather{
+	long money = 999999999999999999L;
+	
+	void wealth() {
+		System.out.println("돈잘범");
+	}
+}
---
+++ Ch09_Extends/src/ch09/extends02/GrandFather.java
@@ -0,0 +1,9 @@
+package ch09.extends02;
+
+public class GrandFather {
+	int handsomeScore = 10;
+	
+	void hansome() {
+		System.out.println("잘생김");
+	}
+}
