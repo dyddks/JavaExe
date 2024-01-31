cb91298722e828a0ed95a66e3324c6f0cae7060d
dyddks <dyddks0115@naver.com>
2024-01-31 15:12:45 +0900

    상속 예제

---
+++ Ch09_Extends/src/ch09/extends01/ExtendsMain.java
@@ -0,0 +1,13 @@
+package ch09.extends01;
+
+public class ExtendsMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child child = new Child();
+		child.hansome();
+		child.wealth();
+		child.play();
+	}
+
+}
---
+++ Ch09_Extends/src/ch09/extends01/GrandFather.java
@@ -0,0 +1,7 @@
+package ch09.extends01;
+
+public class GrandFather {
+	void hansome() {
+		System.out.println("잘생김");
+	}
+}
