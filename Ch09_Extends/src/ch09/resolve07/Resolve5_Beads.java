21407032494a0392f095f5ebf58ef5de2b8d0ac3
dyddks <dyddks0115@naver.com>
2024-02-01 15:21:02 +0900

    주석 추가

--- Ch09_Extends/src/ch09/resolve07/Resolve5_Beads.java
+++ Ch09_Extends/src/ch09/resolve07/Resolve5_Beads.java
@@ -1,5 +1,10 @@
 package ch09.resolve07;
 
+/* 클래스 설계에서 접근권한 설정
+ * 1)필드 -> private
+ * 2)메소드 -> public
+ * 	메소드 중에 내부에서만 사용하는 메소드 -> private
+ * */
 public class Resolve5_Beads {
 	private String name;
 	private int beadsCnt;
