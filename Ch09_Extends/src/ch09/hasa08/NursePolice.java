e7f2d0c4836883cd34e85f85a2d7c12a7d9357a9
dyddks <dyddks0115@naver.com>
2024-02-01 15:44:59 +0900

    포함관계 has a 예제

---
+++ Ch09_Extends/src/ch09/hasa08/NursePolice.java
@@ -0,0 +1,19 @@
+package ch09.hasa08;
+
+public class NursePolice extends Police{
+	private int thermometer;	//체온계 개수
+	
+	public NursePolice(int clothes, int thermometer) {
+		super(clothes);
+		this.thermometer = thermometer;
+	}
+	public void calcThermometer() {
+		if(thermometer>0) {
+			System.out.println("Check 36.5 Temp");
+			thermometer--;
+		}else {
+			System.out.println("No Check Temp");
+		}
+		
+	}
+}
