438c94a4550ecf34b6063f886512c69c52546031
dyddks <dyddks0115@naver.com>
2024-02-01 12:31:20 +0900

    코드 줄바꿈

--- Ch09_Extends/src/ch09/resolve07/Resolve2_Triangle.java
+++ Ch09_Extends/src/ch09/resolve07/Resolve2_Triangle.java
@@ -8,12 +8,15 @@ public class Resolve2_Triangle {
 		this.width = width;
 		this.hight = hight;
 	}
+	
 	public void setWidth(int width) {
 		this.width = width;
 	}
+	
 	public void setHight(int hight) {
 		this.hight = hight;
 	}
+	
 	public double area() {
 		return (double)width*hight/2;
 	}
