2e07aee8f59007818b2467074bda40e76dd4b001
dyddks <dyddks0115@naver.com>
2024-02-02 10:15:54 +0900

    EmployeeMain구현

--- Ch09_Extends/src/ch09/abstract10/EmployeeMain.java
+++ Ch09_Extends/src/ch09/abstract10/EmployeeMain.java
@@ -4,7 +4,8 @@ public class EmployeeMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		EmployeeManeger em = new EmployeeManeger();
+		em.run();
 	}
 
 }
