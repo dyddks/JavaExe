34bf7ef21280c55b54da12f9f83294df5b914dfd
dyddks <dyddks0115@naver.com>
2024-02-05 15:11:36 +0900

    Question2 Answer2_Main 작성

--- Ch09_Extends/src/ch09/resolve14/question2/Answer2.java
+++ Ch09_Extends/src/ch09/resolve14/question2/Answer2.java
@@ -23,9 +23,35 @@ public class Answer2 implements IQuestionAnswer{
 	}
 
 	@Override
-	public void answer(Scanner sc) {
+	public void answer(Scanner sc) throws InterruptedException {
 		sc.nextLine();
 		System.out.println("2번 문제 실행");
+		TabletTester tabletTester = new TabletTester();
+		ITablet[] tabletArr = new ITablet[] {new SamsungTablet(), new LGTablet(), new SonyTablet()};
+		int[] score = new int[tabletArr.length];
+		
+		for(int i=0; i<tabletArr.length; i++) {
+			tabletTester.setTablet(tabletArr[i]);
+			
+			score[i] += tabletTester.movieTest();
+			score[i] += tabletTester.musicTest();
+			score[i] += tabletTester.readBookTest();
+			
+			System.out.printf("총 테스트 점수는 %d입니다.\n", score[i]);
+			System.out.println("----------------------------");
+		}
+		
+		int max = score[0];
+		int maxIdx = 0;
+		for(int i=1; i<score.length; i++) {
+			if(max<score[i]) {
+				max = score[i];
+				maxIdx = i;
+			}
+		}
+		
+		System.out.printf("가장 높은 점수를 받은 태블릿은 %d번째 태블릿이고 점수는 %d입니다.\n", maxIdx+1, max);
+		System.out.println(tabletArr[maxIdx].getClass().getName());
 		
 	}
 
