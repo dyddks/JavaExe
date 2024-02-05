750ba46a7b41d1eff5eff948913600e893b63e54
dyddks <dyddks0115@naver.com>
2024-02-05 14:24:23 +0900

    Answer1 Question1_Main 작성

--- Ch09_Extends/src/ch09/resolve14/question1/Answer1.java
+++ Ch09_Extends/src/ch09/resolve14/question1/Answer1.java
@@ -33,6 +33,16 @@ public class Answer1 implements IQuestionAnswer {
 	public void answer(Scanner sc) {
 		sc.nextLine();
 		System.out.println("1번 문제 실행");
+		PrinterInfo[] print = new PrinterInfo[] {
+				new InkPrinter("S-103", "삼성", "USB", 10, 10),
+				new LaserPrinter("L-103", "LG", "paraller port", 10, 10)};
+
+		for(int i=0; i<print.length; i++) {
+			print[i].viewPrinterInfo();
+			print[i].print();
+			print[i].viewPrinterInfo();
+			System.out.println("-----------------------");
+		}
 
 	}
 
