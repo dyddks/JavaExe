458814817cfe572a5c7ac96e898e4853e27229c5
dyddks <dyddks0115@naver.com>
2024-02-08 11:16:47 +0900

    설명 주석 추가

--- Ch13_Generic/src/ch13/sec01/GenericExample.java
+++ Ch13_Generic/src/ch13/sec01/GenericExample.java
@@ -1,16 +1,27 @@
 package ch13.sec01;
 
+/* Box<String> box1 = new Box<>();
+ * Box<Integer> box2 = new Box<>();
+ * 
+ * 위 처럼 코드를 작성하면 아래처럼 컴파일러는 자동으로 2개의 클래스를 정의하게 된다. 
+ * class Box_String {
+ * 	public String content;
+ * }
+ * class Box_Integer {
+ * 	public Integer content;
+ * }
+ * */
 public class GenericExample {
 
 	public static void main(String[] args) {
-		Box<String> box1 = new Box<>();
-		box1.content = "안녕하세요";
-		String str = box1.content;
+		Box<String> box1 = new Box<>();	// T == String
+		box1.content = "안녕하세요";	// 박싱 : Heap -> Stack
+		String str = box1.content;	// 언박싱 : Stack -> Heap 
 		System.out.println(str);
 		
-		Box<Integer> box2 = new Box<>();
-		box2.content = 100;
-		int value = box2.content;
+		Box<Integer> box2 = new Box<>();	// T == Integer
+		box2.content = 100;			// 박싱
+		int value = box2.content;	// 언박싱
 		System.out.println(value);
 	}
 
