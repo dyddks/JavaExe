aa98e67a2572c91eb4e62a03bef6713ca14c0c36
dyddks <dyddks0115@naver.com>
2024-02-13 17:50:21 +0900

    교재 ch09.sec07.exam03 예제

---
+++ Ch16_Lambda/src/ch16/lambda01/ch09/sec07/exam03/Button.java
@@ -0,0 +1,17 @@
+package ch16.lambda01.ch09.sec07.exam03;
+
+public class Button {
+	public static interface ClickListener{
+		void onClick();
+	}
+	
+	private ClickListener clickListener;
+	
+	public void setClickListener(ClickListener clickListener) {
+		this.clickListener = clickListener;
+	}
+	
+	public void click() {
+		this.clickListener.onClick();
+	}
+}
