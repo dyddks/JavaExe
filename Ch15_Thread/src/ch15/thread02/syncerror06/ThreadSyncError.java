e2d5c85a3c397088a903b0d63a48c4199f86652f
dyddks <dyddks0115@naver.com>
2024-02-13 16:16:31 +0900

    스레드 동기화 예제

---
+++ Ch15_Thread/src/ch15/thread02/syncerror06/ThreadSyncError.java
@@ -0,0 +1,49 @@
+package ch15.thread02.syncerror06;
+
+class Value{
+	private int num = 0;
+	
+	public int getNum() {
+		return num;
+	}
+	
+	public void Increment() {
+		num++;
+	}
+}
+
+class IncThread extends Thread{
+	private Value val;
+	
+	public IncThread(Value val) {
+		this.val = val;
+	}
+	
+	@Override
+	public void run() {
+		for(int i=0; i<100000; i++) {
+			val.Increment();
+		}
+	}
+}
+
+public class ThreadSyncError {
+	public static void main(String[] args) {
+		Value val = new Value();
+		IncThread it1 = new IncThread(val);
+		IncThread it2 = new IncThread(val);
+		it1.start();
+		
+		// 순서대로 했을 때 별 문제없음
+		try {
+			it1.join();
+			it2.start();
+			it2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(val.getNum());
+	}
+}
